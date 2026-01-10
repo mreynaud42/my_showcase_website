58094c6f85bc77344c826ec400a285d0b114e667
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-10 03:35:27 +0100

    add base of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,8 @@
+import "../styles/header.css";
 
 export default function Header() {
-    return <header>Header</header>
+    return <header>
+        <div><a href="/"><img src="/logo/mreynaud.svg" alt="logo mreynaud" /></a></div>
+        <div><img src="/menu.svg" alt="menu" /></div>
+    </header>
 }
