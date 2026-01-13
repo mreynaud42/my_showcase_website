947a8aaa97445d6a3cf2302509a38924cccf95e0
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-13 01:55:31 +0100

    create slider component

---
+++ src/components/SliderCards.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Arrow from "./icon/arrow"
+
+import "../styles/sliderCards.css";
+
+// type ButtonCtaType = {
+//     href: string;
+//     cta: string;
+// }
+
+export default function SliderCards() {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    
+    const cards = [1, 2, 3, 4, 5];
+    
+    return (
+        <div className="slider">
+            <div className="slider-cards"
+                style={{ transform: `translateX(-${currentIndex * 100}%)`,}}>
+                {cards.map((card, index) => (<div className="card" key={index}><p>{card}</p></div>))}
+            </div>
+            <ol className="dots-cards">
+                {cards.map((_, index) => (
+                    <li key={index}
+                        className={`dot ${currentIndex === index ? "is-selected" : ""}`}
+                        onClick={() => setCurrentIndex(index)}>    
+                    </li>
+                ))}
+            </ol>
+            <button className="left"
+                onClick={() => setCurrentIndex(currentIndex === 0 ? cards.length - 1 : currentIndex - 1)}>
+                    <Arrow />
+            </button>
+            <button className="right"
+                onClick={() => setCurrentIndex(currentIndex === cards.length - 1 ? 0 : currentIndex + 1)}>
+                    <Arrow />
+            </button>
+        </div>
+    );
+}
