35abe2678804e233dbb8e59432231632db5c8481
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-14 01:29:27 +0100

    add functional menu

---
+++ src/components/MenuDiv.tsx
@@ -0,0 +1,35 @@
+import Mreynaud from "./logo/Mreynaud"
+import SocialMedia from "./footer/SocialMedia"
+
+import "../styles/menuDiv.css";
+
+type MenuDivType = {
+    className: string;
+};
+
+export default function MenuDiv({ className }: MenuDivType) {
+    return (
+        <div className={`fullscreen-menu ${className}`} >
+            <div className="logo"><a href="/"><Mreynaud/></a></div>
+            <nav className="nav-menu">
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+                <div className="social-media-menu">
+                    <SocialMedia/>
+                </div>
+            </nav>
+        </div>
+    );
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+
+import MenuDiv from "../MenuDiv"
+
 import "../../styles/header/header.css";
 
 export default function Header() {
+    const [isCheck, setIsCheck] = useState(false);
+
+    useEffect(() => {
+        const body: HTMLElement = document.body;
+
+        if (isCheck) {
+            body.classList.add("no-scroll");
+
+        } else {
+            body.classList.remove("no-scroll");
+        }
+
+        return () => body.classList.remove("no-scroll");
+    }, [isCheck]);
+
     return (
         <header>
-            <div><a href="/"><img src="/logo/mreynaud.svg" alt="logo mreynaud" /></a></div>
-            <div><img src="/menu.svg" alt="menu" /></div>
+            <div className="top-bar"><a href="/"><img src="/logo/mreynaud.svg" alt="logo mreynaud" /></a></div>
+            <div className="top-bar">
+                <input type="checkbox" id="menu-toggle" checked={isCheck} onChange={(e) => setIsCheck(e.target.checked)}/>
+                <label htmlFor="menu-toggle" className="menu-button">
+                    <img src="/menu.svg" alt="menu" />
+                </label>
+            </div>
+            <MenuDiv className={`${isCheck ? "check" : ""}`}/>
         </header>
     );
 }
