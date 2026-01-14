9fb1a9b0e327c5cbf1153157a94f8edc39bc135e
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-14 04:15:45 +0100

    fix width section contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <>
             <div className="head"><h1 className="namepage">Contact</h1></div>
-            <section>
+            <section className="contact">
                 <h2>Contact me</h2>
                 <p>I would be delighted to hear from you. Whether you have questions, <strong>an internship opportunity</strong>, or a potential collaboration, please feel free to contact me by email. I will respond as soon as possible.</p>
                 <div className="info-contact">
