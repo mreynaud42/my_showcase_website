94b7f2384b4de2f18ee12e8d0af4aad6c97a68aa
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-15 09:46:41 +0100

    delete link 'a' in marquee footer

--- showcase_website/src/components/footer/Marquee.tsx
+++ showcase_website/src/components/footer/Marquee.tsx
@@ -6,12 +6,12 @@ import "../../styles/components/footer/marquee.css";
 function ElementMarquee() {
     return (
         <div>
-            <span><a href="/"><LogoMreynaud /></a></span>
-            <span><a href="/"><LogoMathisReynaud /></a></span>
-            <span><a href="/"><LogoMreynaud /></a></span>
-            <span><a href="/"><LogoMathisReynaud /></a></span>
-            <span><a href="/"><LogoMreynaud /></a></span>
-            <span><a href="/"><LogoMathisReynaud /></a></span>
+            <span><LogoMreynaud /></span>
+            <span><LogoMathisReynaud /></span>
+            <span><LogoMreynaud /></span>
+            <span><LogoMathisReynaud /></span>
+            <span><LogoMreynaud /></span>
+            <span><LogoMathisReynaud /></span>
         </div>
     );
 }
