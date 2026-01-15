fa2433e420678f3ffe684b8e9ef92c9829efe64f
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-15 10:43:49 +0100

    fix mistake in title

--- showcase_website/src/pages/Resume.tsx
+++ showcase_website/src/pages/Resume.tsx
@@ -7,7 +7,7 @@ export default function Resume() {
         <>
             <div className="head"><h1 className="namepage">Resume</h1></div>
             <section className="resume">
-                <h2>Mathis Reynaud's CV</h2>
+                <h2>Mathis Reynaud's resume</h2>
                 <iframe src="/external_resource/resume.pdf#page=1&zoom=20" width="600" height="848"/>
                 <div className="button">
                     <a href="/external_resource/resume.pdf" className="button-download" download="_Mathis-Reynaud-resume.pdf">
