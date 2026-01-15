f692b4456e0975c9e549d5c7b37b273cc2d400e5
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-15 05:42:26 +0100

    delete button and change div by link of page project

--- src/components/CardProjectHome.tsx
+++ src/components/CardProjectHome.tsx
@@ -1,6 +1,3 @@
-
-import ButtonCTA from "./ButtonCTA"
-
 import type { Project } from "../data/projects"
 
 import "../styles/components/cardProjectHome.css";
@@ -12,21 +9,18 @@ type CardProjectHomeProps = {
 export default function CardProjectHome({ project }: CardProjectHomeProps) {
 
     return (
-        <div className="card-project-home">
+        <a href={`/projects/${project.id}`} className="card-project-home">
             <div className="text">
                 <h3>{project.title}</h3>
                 {project.shortDescription.description.map((p, index) => (
                     <p key={index}>{p}</p>
                 ))}
-                <div className="button">
-                    <ButtonCTA href={`/projects/${project.id}`} cta="Learn more"/>
-                </div>
             </div>
             <div className="img">
                 {project.shortDescription.imgs && (
                     <img src={project.shortDescription.imgs[0]} alt={`Image of the ${project.title} project`} />
                 )}
             </div>
-        </div>
+        </a>
     );
 }
