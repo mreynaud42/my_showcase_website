7f3c4223bb0210473f5dcd58b7b9391cbd558f7f
mreynaud42 <mathisreynaud07@gmail.com>
2026-01-30 14:06:28 +0100

    add paragraph in page contact

--- showcase_website/src/pages/Resume.tsx
+++ showcase_website/src/pages/Resume.tsx
@@ -11,6 +11,7 @@ export default function Resume() {
             <div className="head"></div>
             <section className="resume">
                 <h2>Mathis Reynaud's resume</h2>
+                <p>I am currently seeking a <strong>4 to 6 month internship in software development starting in March 2026</strong>, in order to put my technical skills into practice within a dynamic team. Motivated, rigorous, and curious, I am looking for professional experience that will allow me to make a concrete contribution to software projects while developing my skills in application design, development, and maintenance.</p>
                 <iframe src="/external_resource/resume.pdf#page=1&zoom=20" width="600" height="848"/>
                 <div className="button">
                     <a href="/external_resource/resume.pdf" className="button" download="_Mathis-Reynaud-resume.pdf">
