32b3a9c21752eb087bb4e3623e242aec64367edd
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-04 00:49:06 +0100

    add vercel analytics

--- showcase_website/src/App.tsx
+++ showcase_website/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -23,6 +24,7 @@ function App() {
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
+            <Analytics />
         </BrowserRouter>
     );
 }
