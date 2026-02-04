05bac358143c9068717f8c471c36b1a1c5d6c385
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-05 00:21:50 +0100

    fix button tech and creat button links in page project

--- showcase_website/src/components/ProjectDiv.tsx
+++ showcase_website/src/components/ProjectDiv.tsx
@@ -1,12 +1,44 @@
-import ButtonCTA from "./ButtonCTA"
+import LogoGitHub from "./logo/Git";
 import ButtonTech from "./ButtonTech"
+import ButtonCTA from "./ButtonCTA"
 
 import type { Project } from "../data/projects"
 
 import "../styles/components/projectDiv.css";
+import "../styles/components/projectDivFooter.css";
 
 type ProjectDivProps = {
     project: Project,
+}
+
+function ProjectDivFooter({ project }: ProjectDivProps) {
+    return (
+        <div className="footer">
+            {(project.technologies.length !== 0) && (
+                <div className="technologies">
+                        {project.technologies.map((tech, index) => (
+                            <ButtonTech key={index} technologie={tech}/>
+                        ))}
+                </div>
+            )}
+            {(project.githubUrl || project.demoUrl) && (
+                <div className="links">
+                    {project.githubUrl && (
+                        <div className="icon">
+                            <a href={project.githubUrl} target="_blank"><LogoGitHub /></a>
+                            <span className="tooltip">GitHub</span>
+                        </div>
+                    )}
+                    {project.demoUrl && (
+                        <div className="icon">
+                            <a href={project.githubUrl} target="_blank">Demo</a>
+                            <span className="tooltip">Demo</span>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default function ProjectDiv({ project }: ProjectDivProps) {
@@ -30,25 +62,7 @@ export default function ProjectDiv({ project }: ProjectDivProps) {
             <div className="button">
                 <ButtonCTA href={`/projects/${project.id}`} cta="See my project"/>
             </div>
-            <div className="footer">
-                {(project.technologies.length !== 0) && (
-                    <div className="technologies">
-                            {project.technologies.map((tech, index) => (
-                                <ButtonTech key={index} technologie={tech}/>
-                            ))}
-                    </div>
-                )}
-                {(project.githubUrl || project.demoUrl) && (
-                    <div className="links">
-                        {project.githubUrl && (
-                            <a href={project.githubUrl}>GitHub</a>
-                        )}
-                        {project.demoUrl && (
-                            <a href={project.githubUrl}>Demo</a>
-                        )}
-                    </div>
-                )}
-            </div>
+            <ProjectDivFooter project={project}/>
         </section>
     );
 }
