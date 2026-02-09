f5ce5e7adf3e2492a123eb7b5b211c2f4b6cc388
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-09 09:37:51 +0100

    add i18n (language support)

---
+++ showcase_website/src/i18n/index.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import fr from "./locales/fr.json"
+import en from "./locales/en.json"
+
+export default i18n.use(LanguageDetector).use(initReactI18next).init({
+    resources: {
+        fr: { translation: fr},
+        en: { translation: en},
+    },
+
+    supportedLngs: ["fr", "en"],
+    lng: "en",
+    fallbackLng: "en",
+
+    detection: {
+        order: ["localStorage", "cookie", "navigator"],
+        caches: ["localStorage", "cookie"],
+        lookupLocalStorage: "language",
+        lookupCookie: "language",
+    },
+
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
