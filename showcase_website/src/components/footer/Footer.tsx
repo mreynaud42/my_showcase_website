279a263986a5474533cebb9b98acabd2f25930ea
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-09 09:39:34 +0100

    add traslate french in footer

--- showcase_website/src/components/footer/Footer.tsx
+++ showcase_website/src/components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { Trans, useTranslation } from "react-i18next";
+
 import Marquee from "./Marquee"
 import CardLink from "./CardLink"
 import SocialMedia from "./SocialMedia"
@@ -5,22 +7,21 @@ import SocialMedia from "./SocialMedia"
 import "../../styles/components/footer/footer.css";
 
 export default function Footer() {
+    const { t, i18n } = useTranslation()
+
+    const pages: string[] = ["home", "about", "projects", "contact"];
     return (
         <footer>
             <SocialMedia />
             <nav>
-                <CardLink href="/" title="Home">
-                    <p>A space dedicated to showcasing my skills, experience, and IT projects.</p>
-                </CardLink>
-                <CardLink href="/about" title="About" cta="About me">
-                    <p>As a developer passionate about IT and modern technologies, I present my career path here.</p>
-                </CardLink>
-                <CardLink href="/projects" title="Projects" cta="See my projects">
-                    <p>A selection of personal and professional projects highlighting my technical skills, working methods, and approach to IT development.</p>
-                </CardLink>
-                <CardLink href="/contact" title="Contact" cta="Contact me">
-                    <p>Have a question or a <strong>professional opportunity</strong>? Feel free to contact me to discuss or learn more about my profile.</p>
-                </CardLink>
+                {pages.map((page, i) => (
+                    <CardLink
+                        key={i} href="/" title={t(`${page}.title`)}
+                        cta={i18n.exists(`${page}:title`)? t(`${page}.ctaDefault`) : t("common.ctaDefault")}
+                    >
+                        <p><Trans i18nKey={`${page}.description`} components={{ strong: <strong /> }} /></p>
+                    </CardLink>
+                ))}
             </nav>
             <Marquee />
         </footer>
