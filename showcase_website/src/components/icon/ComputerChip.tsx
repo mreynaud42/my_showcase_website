8223f2b66a56eae70a569aebec90f1c9a11c761b
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-11 18:11:39 +0100

    add icon

---
+++ showcase_website/src/components/icon/ComputerChip.tsx
@@ -0,0 +1,11 @@
+
+export default function ComputerChip() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" fill="currentColor">
+            <path d=" M55 30 H145 A25 25 0 0 1 170 55 V145 A25 25 0 0 1 145 170 H55 A25 25 0 0 1 30 145 V55 A25 25 0 0 1 55 30 Z" fill="none" stroke="currentColor" strokeWidth="12"/>
+            <path d="M83 65 H117 A18 18 0 0 1 135 83 V117 A18 18 0 0 1 117 135 H83 A18 18 0 0 1 65 117 V83 A18 18 0 0 1 83 65 Z" fill="none" stroke="currentColor" strokeWidth="10"/>
+            <path d=" M55 5 H67 V30 H55 Z M94 5 H106 V30 H94 Z M133 5 H145 V30 H133 Z M55 170 H67 V195 H55 Z M94 170 H106 V195 H94 Z M133 170 H145 V195 H133 Z M5 55 H30 V67 H5 Z M5 94 H30 V106 H5 Z M5 133 H30 V145 H5 Z M170 55 H195 V67 H170 Z M170 94 H195 V106 H170 Z M170 133 H195 V145 H170 Z"/>
+        </svg>
+    );
+}
+
