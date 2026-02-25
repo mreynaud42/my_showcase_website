d187b9a12d356803de1d3048a4334658f46cac2f
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-25 16:52:59 +0100

    change select to handmade select

--- showcase_website/src/components/header/LangSelector.tsx
+++ showcase_website/src/components/header/LangSelector.tsx
@@ -1,16 +1,78 @@
+import { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
+import Arrow from "../icon/arrow"
+
 import "../../styles/components/header/langSelector.css";
+
+const languages = [
+    {
+        code: "fr",
+        label: "FR",
+        flag: "/flag_lang/France_language.webp",
+    },
+    {
+        code: "en",
+        label: "EN",
+        flag: "/flag_lang/English_language.webp",
+    },
+];
+
+type LanguageCode = typeof languages[number]["code"];
 
 export default function LangSelector() {
     const { i18n } = useTranslation();
 
+    const [open, setOpen] = useState<boolean>(false);
+    const selectRef = useRef<HTMLDivElement | null>(null);
+
+    const currentLang = languages.find((l) => i18n.language.startsWith(l.code)) ?? languages[0];
+
+    const handleSelect = (code: LanguageCode) => {
+        i18n.changeLanguage(code);
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const target = event.target;
+
+            if (selectRef.current && target instanceof Node && !selectRef.current.contains(target)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div>
-            <select className="lang" value={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
-                <option value="fr">🇫🇷 FR</option>
-                <option value="en">🇬🇧 EN</option>
-            </select>
+        <div className="custom-select" ref={selectRef}>
+            <div className="selected" onClick={() => setOpen((prev) => !prev)}>
+                <img src={currentLang.flag} alt={currentLang.label} />
+                {currentLang.label}
+                <span className="arrow">
+                    <Arrow />
+                </span>
+            </div>
+
+            {open && (
+                <div className="options">
+                    {languages.map((lang) => (
+                        <div
+                            key={lang.code}
+                            className="option"
+                            onClick={() => handleSelect(lang.code)}
+                        >
+                            <img src={lang.flag} alt={lang.label} />
+                            <p>{lang.label}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
