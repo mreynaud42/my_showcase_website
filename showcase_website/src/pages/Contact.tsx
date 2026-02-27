31cda9ae318ef8a8d264fc7665fe118d43cb4bb9
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-27 19:41:20 +0100

    add fr to page contact

--- showcase_website/src/pages/Contact.tsx
+++ showcase_website/src/pages/Contact.tsx
@@ -1,24 +1,27 @@
+import { Trans, useTranslation } from "react-i18next";
 import IconSend from "../components/icon/send"
 
 import "../styles/pages/contact.css";
 import "../styles/components/button.css";
 
 export default function Contact() {
+    const { t } = useTranslation()
+
     return (
         <div className="contact">
             <div className="head"></div>
             <section className="contact">
-                <h2>Contact me</h2>
-                <p>I would be delighted to hear from you. Whether you have questions, <strong>an internship opportunity</strong>, or a potential collaboration, please feel free to contact me by email. I will respond as soon as possible.</p>
+                <h2>{t("contact.sContact.title")}</h2>
+                <p><Trans i18nKey={"contact.sContact.p"} components={{ strong: <strong /> }} /></p>
                 <div className="center">
                     <div className="info-contact">
                         <div className="contact-details">
-                            <h3>Contact details:</h3>
+                            <h3>{t("contact.sContact.detail")}</h3>
                             <a href="mailto:mathisreynaud07@gmail.com">mail: mathisreynaud07@gmail.com</a>
                             <a href="tel:+33768584081">Tel: 07 68 58 40 81</a>
                         </div>
                         <div className="social">
-                            <h3>Social media:</h3>
+                            <h3>{t("contact.sContact.social")}</h3>
                             <a href="https://github.com/mreynaud42" target="_blank">GitHub: github.com/mreynaud42</a>
                             <a href="https://linkedin.com/in/mathis-reynaud" target="_blank">LinkedIn: linkedin.com/in/mathis-reynaud</a>
                         </div>
@@ -26,7 +29,7 @@ export default function Contact() {
                 </div>
                 <div className="button">
                     <a href="mailto:mathisreynaud07@gmail.com" className="button">
-                        <div><p className="text-cta">Send me an email</p></div>
+                        <div><p className="text-cta">{t("contact.sContact.cta")}</p></div>
                         <span><IconSend /></span>
                     </a>
                 </div>
