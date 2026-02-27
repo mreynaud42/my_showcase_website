54514915763c2ea0c87a8daed22458292f8ef85b
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-27 21:02:05 +0100

    add fr to page home

--- showcase_website/src/pages/Home.tsx
+++ showcase_website/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Trans, useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
 
 import ButtonCTA from "../components/ButtonCTA"
@@ -37,6 +38,7 @@ function Hole({ children }: HoleProps) {
 }
 
 export default function Home() {
+    const { t } = useTranslation()
 
     const featuredProjects = projects.filter(project => project.featured);
 
@@ -58,42 +60,41 @@ export default function Home() {
             </div>
             <div className="info">
                 <section className="me">
-                    <h2>Mathis Reynaud</h2>
-                    <p>Currently studying computer science, I specialize in application development. This site presents my profile and my work.</p>
-                    <p>Want to learn more about me?</p>
+                    <h2>{t("home.sAbout.title")}</h2>
+                    <p><Trans i18nKey={"home.sAbout.description"} components={{ strong: <strong /> }} /></p>
+                    <p>{t("home.sAbout.cta")}</p>
                     <div className="button">
-                        <ButtonCTA href="/about" cta="About me"/>
+                        <ButtonCTA href="/about" cta={t("home.sAbout.ctaButton")}/>
                     </div>
                 </section>
                 <section className="me">
-                    <h2>Let's get in touch</h2>
-                    <p>I am open to professional exchanges, <strong>internship opportunities</strong>, and discussions about IT projects.</p>
-                    <p>Want to contact me?</p>
+                    <h2>{t("home.sContact.title")}</h2>
+                    <p><Trans i18nKey={"home.sContact.description"} components={{ strong: <strong /> }} /></p>
+                    <p>{t("home.sContact.cta")}</p>
                     <div className="button">
-                        <ButtonCTA href="/contact" cta="Contact me"/>
+                        <ButtonCTA href="/contact" cta={t("home.sContact.ctaButton")}/>
                     </div>
                 </section>
             </div>
             <section className="projects">
-                <h2>My Featured Projects</h2>
+                <h2>{t("home.sProjects.title")}</h2>
                 <div className="home-slider">
                     <SliderCards>
                         {featuredProjects.map((featuredProject, index) => (
                             <CardProjectHome key={index} project={featuredProject}></CardProjectHome>
                         ))}
                     </SliderCards>
-                    {/* <div className="fade-horizontal"></div> */}
                 </div>
                 <div className="button">
-                    <ButtonCTA href="/projects" cta="See more of my projects"/>
+                    <ButtonCTA href="/projects" cta={t("home.sProjects.ctaButton")}/>
                 </div>
             </section>
             <section className="home-resume">
-                <h2>Resume</h2>
-                <p>I am currently seeking a <strong>4 to 6 month internship in software development starting in March 2026</strong>, in order to put my technical skills into practice within a dynamic team. Motivated, rigorous, and curious, I am looking for professional experience that will allow me to make a concrete contribution to software projects while developing my skills in application design, development, and maintenance.</p>
-                <p>Want to see my resume?</p>
+                <h2>{t("home.sResume.title")}</h2>
+                <p><Trans i18nKey={"home.sResume.description"} components={{ strong: <strong /> }} /></p>
+                <p>{t("home.sResume.cta")}</p>
                 <div className="button">
-                    <ButtonCTA href="/resume" cta="See my resume"/>
+                    <ButtonCTA href="/resume" cta={t("home.sResume.ctaButton")}/>
                 </div>
             </section>
         </div>
