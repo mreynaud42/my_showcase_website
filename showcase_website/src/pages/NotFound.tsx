e461df477cc5d4fe46d7544b130706c58e77d9ea
mreynaud42 <mathisreynaud07@gmail.com>
2026-02-27 21:32:06 +0100

    fix page not found

--- showcase_website/src/pages/NotFound.tsx
+++ showcase_website/src/pages/NotFound.tsx
@@ -1,9 +1,13 @@
 import { useTranslation } from "react-i18next";
 
+import "../styles/pages/notFound.css";
+
 export default function NotFound() {
     const { t } = useTranslation()
 
     return (
-        <h1>{t("notFound")}</h1>
+        <div className="not-found">
+            <h1>{t("notFound")}</h1>
+        </div>
     );
 }
