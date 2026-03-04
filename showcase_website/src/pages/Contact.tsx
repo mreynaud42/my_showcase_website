b00fff7c7974cd07c8f78e02c840a316af80e63f
mreynaud42 <mathisreynaud07@gmail.com>
2026-03-04 19:49:13 +0100

    fix: coarse link contact

--- showcase_website/src/pages/Contact.tsx
+++ showcase_website/src/pages/Contact.tsx
@@ -17,13 +17,13 @@ export default function Contact() {
                     <div className="info-contact">
                         <div className="contact-details">
                             <h3>{t("contact.sContact.detail")}</h3>
-                            <a href="mailto:mathisreynaud07@gmail.com">mail: mathisreynaud07@gmail.com</a>
-                            <a href="tel:+33768584081">Tel: 07 68 58 40 81</a>
+                            <a className="button-coarse" href="mailto:mathisreynaud07@gmail.com">mail: mathisreynaud07@gmail.com</a>
+                            <a className="button-coarse" href="tel:+33768584081">Tel: 07 68 58 40 81</a>
                         </div>
                         <div className="social">
                             <h3>{t("contact.sContact.social")}</h3>
-                            <a href="https://github.com/mreynaud42" target="_blank">GitHub: github.com/mreynaud42</a>
-                            <a href="https://linkedin.com/in/mathis-reynaud" target="_blank">LinkedIn: linkedin.com/in/mathis-reynaud</a>
+                            <a className="button-coarse" href="https://github.com/mreynaud42" target="_blank">GitHub: github.com/mreynaud42</a>
+                            <a className="button-coarse" href="https://linkedin.com/in/mathis-reynaud" target="_blank">LinkedIn: linkedin.com/in/mathis-reynaud</a>
                         </div>
                     </div>
                 </div>
